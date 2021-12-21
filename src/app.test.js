5c4395c2e82dad16682fb52727e9a265352f8405
Pedro Antônio <gnx@tuta.io>
2021-12-21 00:13:09 -0300

    test simulate loging

--- src/app.test.js
+++ src/app.test.js
@@ -1,22 +1,52 @@
 const request = require('supertest')
 const App = require('./app')
 
-describe('GET /', () => {
+describe('Route /', () => {
     const app = new App()
-    it('should return 200', function (done) {
+    it('200 should return nothing', function (done) {
         request(app.server).get('/')
             .expect(200)
             .end(done)
     });
 });
 
-describe('POST /users', () => {
+describe('Route /users', () => {
     const app = new App()
-    it('should return json 200', function (done) {
+    it('200 should return json', function (done) {
         request(app.server).post('/users')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200)
+            .end(done)
+    });
+});
+
+describe('Route /login', () => {
+    const app = new App();
+    
+    it('200 should accept correct credentials', function (done) {
+        const data = {
+            username: 'admin',
+            password: 'admin'
+        }
+        request(app.server).post('/login')
+            .send(data)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect(200)
+            .end(done)
+    });
+
+    it('401 should deny wrong credentials', function (done) {
+        const data = {
+            username: 'Admin',
+            password: 'admIn'
+        }
+        request(app.server).post('/login')
+            .send(data)
+            .set('Accept', 'application/json')
+            .set('Content-Type', 'application/json')
+            .expect(401)
             .end(done)
     });
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
 
-router.get('/', (_req, res) => {
+router.use(express.json());
+
+router.get('/', (req, res) => {
     res.status(200).send();
+});
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if(username === 'admin' && password === 'admin') {
+        res.status(200).send();
+    } else {
+        res.status(401).send();
+    }
 });
 
 router.post('/users', (_req, res) => {
